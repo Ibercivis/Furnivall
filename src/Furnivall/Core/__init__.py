b64f1f497e9e56b40c259eeb80c6fac4acfc9871
David Francos <opensource@davidfrancos.net>
2011-05-11 15:41:19 +0200

    Added views.py (empty) to plugins

---
+++ src/Furnivall/Core/__init__.py
@@ -0,0 +1,1 @@
+__all__=['Assignment', 'common', 'Jobs', 'WorkUnit']
--- src/Furnivall/Furnivall.py
+++ src/Furnivall/Furnivall.py
@@ -1,1 +1,7 @@
+from Core import *
+
+class main():
+    def __init__(self):
+        # We've got all in the core, but nothing about the views nor plugin support really implemented!!!
+
 
