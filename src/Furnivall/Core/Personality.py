1cec42ffc4d1d078d4de86aaf19faf5f97d4f680
XayOn <opensource@davidfrancos.net>
2011-06-16 16:02:04 +0200

    Lil bugfixes

--- src/Furnivall/Core/Personality.py
+++ src/Furnivall/Core/Personality.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import collections
 import tornado.session
+import uuid
 """
     Personality objects.
 """
@@ -9,25 +10,37 @@ class Personality(object):
         self.host=host
         self.user=user
 
-class Volunteer(Personality):
+class Researcher(Personality):
     def __init__(self,  host=False, user=False):
-        # TODO Change the method volunteers work. Should be individual volunteers, by sessions, not by-task volunteers.
         """
-            Volunteer object. Conttains host, user, current_tasks and completed_tasks for a user
+            Researcher object object. Conttains host, user, current_tasks and completed_tasks for a user
             TODO: store everything about it in a datbase. Add auth to somewhere.
         """
-        super(Volunteer, self).__init__(user, host)
+        super(self.__class__, self).__init__(user, host)
         self.current_tasks=collections.deque()
         self.completed_tasks=collections.deque()
+        self.initialize_views=( getattr(getattr(Views, viewfile), self.conf('enabled_views', viewfile) )(self) for viewfile in self.config.options('enabled_views') if self.view_ )
+        self.jobs=[ Jobs.job(view, getattr(getattr(Plugins, view.plugin), view.class_)()) for view in self.initialize_views ]
 
     def set_data(self, host, user, id_):
         self.host=host
         self.user=user
         self.id_=id_
 
-class Researcher(Personality):
-    def __init__(self, host, user):
+class Volunteer(Personality):
+    def __init__(self,  host=False, user=False):
+        # TODO Change the method volunteers work. Should be individual volunteers, by sessions, not by-task volunteers.
         """
+            Volunteer object. Conttains host, user, current_tasks and completed_tasks for a user
         """
-        super(Researcher, self).__init__(user, host) 
-        self.jobs=[]
+        super(Volunteer, self).__init__(user, host)
+        self.current_tasks=collections.deque()
+        self.completed_tasks=collections.deque()
+        self.session_id=self.get_session_id()
+
+    def get_session_id(self):
+        return uuid.uuid4()
+
+    def set_data(self, host, user, id_):
+        self.host=host
+        self.user=user
--- src/Furnivall/Furnivall.py
+++ src/Furnivall/Furnivall.py
@@ -7,6 +7,7 @@ from Core.common import CommonFunctions
 import tornado.web
 import tornado.ioloop
 from Core.common import log
+import uuid
 
 class ViewManager(tornado.web.RequestHandler):
     def plug_view(self, viewfile, view):
@@ -37,31 +38,48 @@ class main(CommonFunctions):
         global created_jobs
         created_jobs=[] # Don't delete, when views are created, they need a created_jobs argument in the creator.
         self.read_config()
-        self.initialize_views=( getattr(getattr(Views, viewfile), self.conf('enabled_views', viewfile) )(self) for viewfile in self.config.options('enabled_views') )
+
         log('Loading Furnivall (Creating jobs)')
-        created_jobs=[ { view.name : Jobs.job(view, getattr(getattr(Plugins, view.plugin), view.class_)()) } for view in self.initialize_views ]
+        researchers=self.InitializeResearchers()
+
+    def InitializeResearchers(self):
+        return    
+
+
 
     class Scheduler(ViewManager):
         """
             Assigns a volunteer a specific task
             WARN, NOTE: This might be loosing performance and scalability!
         """
+
         def assign_session_to_user(self, volunteer):
-            return volunteer.set_data(self.session.host, self.session.user, self.session.session_id) # TODO Use real session data from the user.
+            auuid=uuid.uuid4()
+            self.session.user_id=auuid # Assign user id to session.
+            return volunteer.set_data(self.session.host, self.session.user, auuid )
 
         def assign_task(self, view):
-            # TODO: check if the user is already doing that task?
+            free_task=self.getfreetask()
+            if get_current_volunteer() is free_task.volunteer: 
+                log('[Debug] Not creating user, not return task... User is already doing that task, something failed!')
+                return
             log('[Debug] Creating volunteer object and assigning it to a task.')
             assign_session_to_user(self.getfreetask(view).volunteer)
 
         def getworkunit(self):
             return WorkUnit.workunit() # TODO get workunit.
 
+        def get_current_volunteer(self):
+            if not self.session.session_id:
+                return assign_session_to_user
+            else:
+                return self.volunteers[self.session.session_id] # FIXME Make volunteers pool!!!
+
         def getfreetask(self, view):
             # TODO: change ScoreMatch in workunit to call the plugin/view's compatibility class. TODO: Make a view's compatibility class
-            for job in created_jobs:
+            for job in created_jobs: # TODO: If we're under a researcher container, how whould this be? 
                 for view, job in job:
-                    if job.viewObject.check_view(volunteer): # Get volunteer object here! TODO
+                    if job.viewObject.check_view(get_current_volunteer):
                         wk=self.getworkunit()
                         task=[sort(task, key=lambda t: t.ScoreMatch()) for task in wk.tasks if not task.volunteer.session_id ][0] # Get the best task ordered by ScoreMatch if it has not a volunteer assigned
                         if task: return task
@@ -70,14 +88,10 @@ class main(CommonFunctions):
 
         def get_done_tasks(self):
             atasks=[]
-            volunteer=self.get_volunteer(self.session)
+            volunteer=self.get_current_volunteer()
             for workunit in self.find_volunteer_workunits(volunteer):
                 atasks.extend([task in self.find_volunteer_tasks(volunteer, workunit.tasks_ok, workunit.tasks_fail)])
             return atasks
-
-        def get_volunteer(self, session):
-            # TODO Change the method volunteers work. Should be individual volunteers, by sessions, not by-task volunteers.
-            return self.volunteers[session]
 
         def find_volunteer_workunits(self, volunteer):
             for job in created_jobs:
@@ -95,7 +109,10 @@ if __name__ == "__main__":
     """
     m=main()
     main_urls=("/view/([^/]+)", m.Scheduler)
-    urls=[ b for b in [ a[a.keys()[0]].viewObject.urls for a in created_jobs ]][0]
+    try:
+        urls=[ b for b in [ a[a.keys()[0]].viewObject.urls for a in created_jobs ]][0]
+    except:
+        urls=[]
     urls.append(main_urls)
     settings={'session_storage': 'mongodb:///tornado_sessions' }
     log('Starting web server with urls %s' %(urls))
